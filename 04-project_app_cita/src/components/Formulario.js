59aaea9a3b624660eb6d8131af269629a1e36d44
josmig <pantigosomiguel59@gmail.com>
2021-03-23 21:07:38 -0500

    creando state para citas , obteniendo los datos de los inputs

--- 04-project_app_cita/src/components/Formulario.js
+++ 04-project_app_cita/src/components/Formulario.js
@@ -1,19 +1,44 @@
-import React, { Fragment } from 'react';
-
+import React, { Fragment, useState} from 'react';
 
 const Formulario = () =>{
 
+    //Creación de state para formulario (Citas)
+    
+    const [cita,actualizarCita] = useState({
+        mascota: '',
+        propietario:'',
+        sintomas:'',
+        fecha:'',
+        hora:''        
+    });
+
+    //funcion que se ejecuta cada vez que el usuario escribe en un input
+    const handleChange = e =>{
+        /* console.log(e.target.name); */ //con esto mapeamos los campos con el state
+        actualizarCita({
+            ...cita,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    //Extraer los valores
+    const {mascota,propietario,sintomas,fecha,hora} = cita;
+
+
     return(
         <Fragment>
-            {/* <h1>Formulario de Registro</h1> */}
+            {/* <h1>Formulario de Registro</h1> */}ss
             <h2>Crear cita</h2>
             <form>
                 <label>Nombre Mascota</label>    
                 <input 
                     type="text"
                     name="mascota"
-                    className="u-full-width"
+                    className="u-full-width"                    
                     placeholder="Nombre mascota"
+                    onChange={handleChange}
+                    /* Esto nos permitira mas adelante resetear el form */
+                    value={mascota}
                 />
                 <label>Nombre dueño</label>    
                 <input 
@@ -21,26 +46,35 @@ const Formulario = () =>{
                     name="propietario"
                     className="u-full-width"
                     placeholder="Nombre de la mascota"
+                    onChange={handleChange}
+                    value={propietario}
                 />
 
                 <label>Sintomas | Problemas</label>    
                 <textarea
                     name="sintomas"
                     className="u-full-width"
-                    placeholder="Sintomas de la mascota">
+                    placeholder="Sintomas de la mascota"
+                    onChange={handleChange}
+                    value={sintomas}>                        
+                    
                 </textarea>
 
                 <label>Fecha de Alta</label>    
                 <input 
                     type="date"
                     name="fecha"
-                    className="u-full-width"                    
+                    className="u-full-width"
+                    onChange={handleChange}
+                    value={fecha}
                 />
                 <label>Hora</label>    
                 <input 
                     type="time"
                     name="hora"
-                    className="u-full-width"                    
+                    className="u-full-width"
+                    onChange={handleChange}
+                    value={hora}
                 />
 
                 <button
